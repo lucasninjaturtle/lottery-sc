09c307ab9280189bbcda42f412a629b8b3cb3b32
Lucas <lucas.aguero@bluelabellabs.com>
2022-10-25 07:02:42 -0300

    lottery

---
+++ test/Lottery.t.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "../src/Lottery.sol";
+
+contract LoteryTest is Test {
+    Lottery public lottery;
+
+    
+}
